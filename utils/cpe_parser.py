96a04b0663b17305a5d510d7c4dbd63ab5b7a657
PrettyDragonfly <bourcamille@proton.me>
2025-08-21 16:36:44 +0200

    nlp function improve with cache

--- utils/cpe_parser.py
+++ utils/cpe_parser.py
@@ -5,21 +5,22 @@ import os
 
 class CPE:
 
-    FILE_PATH_BRUT = "../data/nvdcpe/*.json"
-    FILE_PATH_PRETTY = "../data/nvdcpe_pretty/"
+    FILE_PATH_BRUT = "data/nvdcpe/*.json"
+    FILE_PATH_PRETTY = "data/nvdcpe_pretty/"
+    CACHE_FILE = "data/products_cache.json"
 
     def __init__(self):
         pass
 
     def reformat(self):
 
-        # Création du dossier de sortie
+        # Output directory creation
         os.makedirs(self.FILE_PATH_PRETTY, exist_ok=True)
 
         for file_path in glob.glob(self.FILE_PATH_BRUT):
             with open(file_path, "r", encoding="utf-8") as f:
                 data = json.load(f)
-            new_file_path = file_path.replace("../data/nvdcpe/", "").replace(".json","")
+            new_file_path = file_path.replace("data/nvdcpe/", "").replace(".json","")
 
             with open(self.FILE_PATH_PRETTY+new_file_path+"_pretty.json", "w",
                    encoding="utf-8") as f:
@@ -28,13 +29,17 @@ class CPE:
     # Convert cpe database to NLP Model
     def build_model(self):
 
+        # Si un cache existe déjà
+        if os.path.exists(self.CACHE_FILE):
+            with open(self.CACHE_FILE, "r", encoding="utf-8") as f:
+                return json.load(f)
+
         products = []
 
         # Parcours de tous les fichiers JSON de la base
         for file_path in glob.glob(self.FILE_PATH_PRETTY+"*.json"):
             with open(file_path, "r", encoding="utf-8") as f:
                 data = json.load(f)
-                print(data.keys())
 
                 for item in data.get("products", []):
                     cpe_info = item.get("cpe", {})
@@ -42,4 +47,9 @@ class CPE:
                     title_entries = cpe_info.get("titles", [])
                     title = next((t["title"] for t in title_entries if t["lang"] == "en"), "")
                     products.append({"cpe_name": cpe_name, "title": title})
-                print(products)
+
+        # Save products in a file
+        with open(self.CACHE_FILE, "w", encoding="utf-8") as f:
+            json.dump(products, f, indent=4, ensure_ascii=False)
+
+        return products
--- utils/nlp.py
+++ utils/nlp.py
@@ -1,10 +1,36 @@
-import json
 import spacy
-from spacy.matcher import Matcher
+from utils.cpe_parser import CPE
+import os
 
-# Chargement du modèle
-nlp = spacy.blank("en")
-#nlp = spacy.load("../data/patterns.json")
+class NLP:
 
-# Créer le matcher
-matcher = Matcher(nlp.vocab)
+    nlp = spacy.blank("en")
+    my_cpe = CPE()
+    MODEL_PATH = "data/nlp_model_with_cpe_patterns"
+
+    def __init__(self):
+        pass
+
+    def create_model(self):
+        # If model exists
+        if os.path.exists(self.MODEL_PATH):
+            print(f"Modèle existant trouvé, chargement depuis {self.MODEL_PATH}")
+            self.nlp = spacy.load(self.MODEL_PATH)
+            return
+
+        ruler = self.nlp.add_pipe("entity_ruler")
+
+        # Load products
+        products = self.my_cpe.build_model()
+
+        patterns = [{"label": "PRODUCT", "pattern": p["title"]} for p in products if p["title"]]
+        ruler.add_patterns(patterns)
+
+        print(f"✅ {len(patterns)} patterns ajoutés")
+
+        # Save model
+        self.nlp.to_disk(self.MODEL_PATH)
+
+    def analyze(self, text):
+        doc = self.nlp(text)
+        print([(ent.label_, ent.text) for ent in doc.ents])
