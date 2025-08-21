b9a6d507f518c393e2bd6279cb748fb71a23ac1e
Camille Pro <camille_pro@Mac.fritz.box>
2025-08-21 09:14:40 +0200

    first draft nlp

--- utils/nlp.py
+++ utils/nlp.py
@@ -1,5 +1,10 @@
 import json
 import spacy
-from spacy.matcher import PhraseMatcher
+from spacy.matcher import Matcher
 
-nlp=spacy.load(my_model)
+# Chargement du modèle
+nlp = spacy.blank("en")
+#nlp = spacy.load("../data/patterns.json")
+
+# Créer le matcher
+matcher = Matcher(nlp.vocab)
