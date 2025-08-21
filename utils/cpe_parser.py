5ed7863fcdcac85ec08b6a1b08e7abe69ba47ffb
PrettyDragonfly <bourcamille@proton.me>
2025-08-21 14:58:21 +0200

    build products model function

--- utils/cpe_parser.py
+++ utils/cpe_parser.py
@@ -25,3 +25,21 @@ class CPE:
                    encoding="utf-8") as f:
                  json.dump(data, f, indent=4, ensure_ascii=False)
 
+    # Convert cpe database to NLP Model
+    def build_model(self):
+
+        products = []
+
+        # Parcours de tous les fichiers JSON de la base
+        for file_path in glob.glob(self.FILE_PATH_PRETTY+"*.json"):
+            with open(file_path, "r", encoding="utf-8") as f:
+                data = json.load(f)
+                print(data.keys())
+
+                for item in data.get("products", []):
+                    cpe_info = item.get("cpe", {})
+                    cpe_name = cpe_info.get("cpeName")
+                    title_entries = cpe_info.get("titles", [])
+                    title = next((t["title"] for t in title_entries if t["lang"] == "en"), "")
+                    products.append({"cpe_name": cpe_name, "title": title})
+                print(products)
